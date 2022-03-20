18f212c402b928d28253c4210d8fb977a87a866a
Luca_721 <g.m.m.meens@gmail.com>
2022-03-20 16:27:31 +0100

    Changed all names from Dutch to the English translation in the now Repair class, used to be Reperatie.

---
+++ src/main/java/com/luca/AutogarageGianlucaMeens/Reperatie/Repair.java
@@ -0,0 +1,56 @@
+package com.luca.AutogarageGianlucaMeens.Reperatie;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Repairs")
+public class Repair {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    @Column(name = "damage")
+    String damage;
+
+    @Column(name = "costs")
+    double costs;
+
+    @Column(name = "price")
+    double price;
+
+    //setters
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setDamage(String damage) {
+        this.damage = damage;
+    }
+
+    public void setCosts(double costs) {
+        this.costs = costs;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    //getters
+    public long getId() {
+        return id;
+    }
+
+    public String getDamage() {
+        return damage;
+    }
+
+    public double getCosts() {
+        return costs;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
+
