9a58296134bb3f006e3f4e660811f1b4a851c754
Luca_721 <g.m.m.meens@gmail.com>
2022-03-20 16:24:44 +0100

    Changed all names from Dutch to the English translation in the now Part class, used to be onderdeel.

---
+++ src/main/java/com/luca/AutogarageGianlucaMeens/Onderdeel/part.java
@@ -0,0 +1,65 @@
+package com.luca.AutogarageGianlucaMeens.Onderdeel;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "parts")
+public class part {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(name = "name")
+    String name;
+
+    @Column(name = "cost")
+    double cost;
+
+    @Column(name = "salesPrice")
+    double salesprice;
+
+    @Column(name = "stock")
+    int stock;
+
+    //setters
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public void setSalesprice(double salesprice) {
+        this.salesprice = salesprice;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    // getters
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public double getSalesprice() {
+        return salesprice;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+}
