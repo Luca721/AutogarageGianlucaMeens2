65fefcfa6bc9d5b80f33036d312a14d6799deaf6
Luca_721 <46169280+Luca721@users.noreply.github.com>
2022-08-25 22:18:27 +0200

    car test gefixt

--- src/test/java/com/luca/AutogarageGianlucaMeens/ControllerTest/CarControllerTest.java
+++ src/test/java/com/luca/AutogarageGianlucaMeens/ControllerTest/CarControllerTest.java
@@ -15,7 +15,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -36,15 +35,12 @@ public class CarControllerTest
     private MockMvc mockMvc;
 
     @MockBean
-    private CarController carController;
-
-    @MockBean
     private CarRepository carRepository;
 
     private ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
-    @WithMockUser(username = "super", password = "super123")
+
      void shouldGetAllCars() throws  Exception{
         List<Car> cars = new ArrayList<>(
                 Arrays.asList(  new Car(1, "abcdef", "audi", "a3", 2002),
@@ -57,12 +53,11 @@ public class CarControllerTest
     }
 
     @Test
-    @WithMockUser(username = "super", password = "super123")
     void shouldGetOneCar() throws  Exception{
         long id = 1;
         Car car = new Car(id, "abcdef", "audi", "a3", 2002);
         when(carRepository.findById(id)).thenReturn(Optional.of(car));
-        mockMvc.perform(get("/Autos/allAutos/1", id))
+        mockMvc.perform(get("/Autos/Auto/1", id))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(id))
                 .andExpect(jsonPath("$.licensePlate").value(car.getLicensePlate()))
@@ -73,24 +68,23 @@ public class CarControllerTest
     }
 
     @Test
-    @WithMockUser(username = "super", password = "super123")
     void shouldCreateCar() throws Exception {
-        Car car = new Car(1, "abcdef", "audi", "a3", 2002);
-        mockMvc.perform(post("/api/tutorials").contentType(MediaType.APPLICATION_JSON)
+        long id = 1;
+        Car car = new Car(id, "abcdef", "audi", "a3", 2002);
+        mockMvc.perform(post("/Autos/newAuto").contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(car)))
-                .andExpect(status().isCreated())
+                .andExpect(status().isOk())
                 .andDo(print());
     }
 
     @Test
-    @WithMockUser(username = "super", password = "super123")
-    void shouldUpdateTutorial() throws Exception {
+    void shouldUpdateCar() throws Exception {
         long id = 1;
         Car car = new Car(1, "abcdef", "audi", "a3", 2002);
         Car updatedcar = new Car(1, "abcdef", "audi", "a3", 2005);
         when(carRepository.findById(id)).thenReturn(Optional.of(car));
         when(carRepository.save(any(Car.class))).thenReturn(updatedcar);
-        mockMvc.perform(put("/api/tutorials/{id}", id).contentType(MediaType.APPLICATION_JSON)
+        mockMvc.perform(put("/Autos/Auto/1", id).contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(updatedcar)))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.id").value(id))
