7f029fe1e4ed1dac56e06c69bad0a8f972ffff36
Luca_721 <46169280+Luca721@users.noreply.github.com>
2022-08-25 22:21:14 +0200

    part first letter upercase refractor

--- src/main/java/com/luca/AutogarageGianlucaMeens/Part/PartRepository.java
+++ src/main/java/com/luca/AutogarageGianlucaMeens/Part/PartRepository.java
@@ -2,5 +2,5 @@ package com.luca.AutogarageGianlucaMeens.Part;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PartRepository extends JpaRepository<part, Long> {
+public interface PartRepository extends JpaRepository<Part, Long> {
 }
